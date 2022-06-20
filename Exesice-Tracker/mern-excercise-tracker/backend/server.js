ed95bff17770f516a0c4dcb1479bdc58b8d606fa
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-20 13:06:18 -0700

    LMS with Back End

--- Exesice-Tracker/mern-excercise-tracker/backend/server.js
+++ Exesice-Tracker/mern-excercise-tracker/backend/server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-/*var mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', {
-
-   email : String ,
-   pwd : String
-
-});*/
-
+const app = express();
 require('dotenv').config();
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); 
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL, { 
+    useCreateIndex: true, 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    //useFindAndModify: false, 
+
+/*}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')*/
+    });
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
