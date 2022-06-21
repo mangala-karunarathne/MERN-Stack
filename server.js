b0074df5e3f422587eb6f72b7038658086b30b94
Mangala Karunarathne <106872883+mangala-karunarathne@users.noreply.github.com>
2022-06-21 10:48:52 -0700

    Connected to Mongodb 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
